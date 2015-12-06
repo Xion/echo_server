34ee87599855530cb32ce91f2cdae977986864d5
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-12-05 19:50:17 -0800

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate getopts;
+
+
+use getopts::Options;
+use std::env;
+
+
+/// Print the instructions about invoking the program from the command line.
+fn print_usage(program: &str, opts: Options) {
+    println!("{}", opts.usage(&format!("Usage: {} [options]", program)));
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut options = Options::new();
+    options.optflag("h", "help", "Show this usage message.");
+
+    let args = match options.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(e) => { panic!(e.to_string()) }
+    };
+    if args.opt_present("h") {
+        print_usage(&program, options);
+        return;
+    }
+
+    println!("Hello world");
 }
